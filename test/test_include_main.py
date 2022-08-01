472d36c2380ebdd97f2dc74ac64a756ed9c38dc7
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-07-31 22:56:48 -0700

    Test for binary option

--- test/test_include_main.py
+++ test/test_include_main.py
@@ -1,7 +1,13 @@
 
+def _assert_main_exists(result):
+	main_file = result.project_path / result.project_path.name / "__main__.py"
+	return main_file.is_file()
+
 def test_includes_main_by_default(cookies):
 	result = cookies.bake()
-	assert result.project_path.name == "best_practices"
-	main_file = result.project_path / result.project_path.name / "__main__.py"
-	assert main_file.is_file()
+	assert _assert_main_exists(result)
 
+def test_no_main_when_passed_n(cookies):
+	result = cookies.bake(extra_context={"binary":"n"})
+	assert not _assert_main_exists(result)
+	result = cookies.bake(extra_context={"binary":"N"})
