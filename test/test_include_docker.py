48dc0aac57847634d03e3c0865f10419a07bf8e6
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-08-02 20:02:43 -0700

    Test for properly including docker files

--- test/test_include_docker.py
+++ test/test_include_docker.py
@@ -6,6 +6,15 @@ def _test_docker_do_not_exists(result):
 	for file in _docker_files(result.project_path):
 		assert not file.is_file()
 
+def _test_docker_exists(result):
+	for file in _docker_files(result.project_path):
+		assert file.is_file()
+
 def test_does_not_include_docker_by_default(cookies):
 	result = cookies.bake()
 	_test_docker_do_not_exists(result)
+
+def test_does_include_docker(cookies):
+	for i in ["y", "Y"]:
+		result = cookies.bake(extra_context={"add_docker":i})
+		_test_docker_exists(result)
