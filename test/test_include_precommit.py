3339fec8c338b441cb15a3324ac71ecb5978633a
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-08-03 21:20:42 -0700

    Test for not including precommit dep

--- test/test_include_precommit.py
+++ test/test_include_precommit.py
@@ -1,4 +1,7 @@
 
+import re
+from functools import reduce
+
 def _includes_precommit_file(result):
 	return result.project_path / ".pre-commit-config.yaml"
 
@@ -10,3 +13,15 @@ def test_arg_not_include_file(cookies):
 	for i in ["n", "N"]:
 		result = cookies.bake({"precommit":i})
 		assert not _includes_precommit_file(result).is_file()
+
+def test_no_precommit_dependecy(cookies):
+	result = cookies.bake()
+	rexp = re.compile('^precommit = "*"$')
+	pipfile = open(result.project_path / "Pipfile")
+	pip_lines = pipfile.readlines()
+	pip_result = map(lambda l: l.strip(), pip_lines)
+	pip_result = map(lambda l: rexp.match(l), pip_result)
+	pip_result = map(lambda l: l is not None, pip_result)
+	pip_result = any(pip_result)
+	pipfile.close()
+	assert pip_result
