20f409b96bd166947c457da1768547254a1c3ad2
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-08-04 22:52:12 -0700

    Test for including github action

--- test/test_github_action.py
+++ test/test_github_action.py
@@ -5,3 +5,7 @@ def _get_github_dir(result):
 def test_remove_github_dir(cookies):
 	result = cookies.bake()
 	assert not _get_github_dir(result).is_dir()
+
+def test_include_github_dir(cookies):
+	result = cookies.bake({"github_action":"y"})
+	assert _get_github_dir(result).is_dir()
