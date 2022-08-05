20ca00ed118cbdf33ac1886f68cefbd3ca8aa5fa
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-08-04 23:05:12 -0700

    Test that github action requires docker

--- test/test_github_action.py
+++ test/test_github_action.py
@@ -7,5 +7,10 @@ def test_remove_github_dir(cookies):
 	assert not _get_github_dir(result).is_dir()
 
 def test_include_github_dir(cookies):
-	result = cookies.bake({"github_action":"y"})
+	result = cookies.bake({"add_docker":"y", "github_action":"y"})
 	assert _get_github_dir(result).is_dir()
+
+def test_include_github_fails_without_docker(cookies):
+	result = cookies.bake({"add_docker":"n", "github_action":"y"})
+	assert result.exit_code != 0
+	assert result.exception is not None
